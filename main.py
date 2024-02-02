13a6be574dee5d3b15115a34fb47708614bb46f6
matrecole <144698344+matrecole@users.noreply.github.com>
2024-02-01 20:53:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import random
 SCRN_W = 800 # largeur de l'ecran de jeu
 SCRN_H = 600 # hauteur de l'ecran de jeu
 
+COLORS = [arcade.color.BLUE, arcade.color.RED, arcade.color.WHITE, arcade.color.BLUE_VIOLET, arcade.color.CANDY_APPLE_RED, arcade.color.PERSIAN_ROSE, arcade.color.BOYSENBERRY, arcade.color.CHESTNUT, arcade.color.YELLOW_ORANGE] # tuple pour les couleurs
+
 
 class MyGame(arcade.Window):
     def __init__(self):
@@ -42,79 +44,88 @@ class MyGame(arcade.Window):
 
             self.rectangles.append(rect) # ajouter le rectangle au groupe
 
+    def on_update(self, delta_time: float):
+        for circls in self.circles: # pour chaque cercles dans le groupe
+            circls.update() # mettre a jour la position du cercle sur l'ecran
+
+        for rects in self.rectangles: # pour chaque rectangle dans le groupe
+            rects.update() # mettre a jour la position du rectangle sur l'ecran
+
 #######################
 
 class Balle:
     def __init__(self):
         self.x = None # les positions de la balle sont definis par la souris
         self.y = None
-        self.changex = 5 # vitesse du cercle en x
-        self.changey = 5 # vitesse du cercle en y
+        self.changex = random.randint(1,5) # vitesse du cercle en x aléatoire entre 1 et 5
+        self.changey = random.randint(1,5) # vitesse du cercle en y aléatoire entre 1 et 5
 
         self.rayon = random.randint(10, 30) # generer un rayon au hasard entre 10 et 30
-        self.color = arcade.color # cherche les couleurs dans la librairie arcade
+        self.color = random.choice(COLORS) # définir une couleur au hasard dans le tuple
 
     def update(self):
         self.x += self.changex
         self.y += self.changey
 
-        if self.x > SCRN_W - self.rayon: # si la position en x va plus loin que la bordure de l'ecran
-            self.x = SCRN_W - self.rayon
-            self.changex * -1 # changer la direction pour l'opposer
+        if self.x > SCRN_W - self.rayon: # si le cercle va plus loin que la bordure droite de l'ecran
+            self.x = SCRN_W - self.rayon # empèche le cercle de sortir
+            self.changex *= -1 # changer la direction pour l'opposé
 
-        if self.x < self.rayon: # si la position en x va plus loin que la bordure de l'ecran
+        if self.x < self.rayon: # si le cercle va plus loin que la bordure gauche de l'ecran
             self.x = self.rayon
-            self.changey * -1
+            self.changex *= -1
 
-        if self.y > SCRN_H - self.rayon: # si la position en y va plus loin que la bordure de l'ecran
+        if self.y > SCRN_H - self.rayon: # si le cercle va plus loin que la bordure en haut de l'ecran
             self.y = SCRN_H - self.rayon
-            self.changey * -1
+            self.changey *= -1
 
-        if self.y < self.rayon: # si la position en y va plus loin que la bordure de l'ecran
+        if self.y < self.rayon: # si le cercle va plus loin que la bordure en bas de l'ecran
             self.y = self.rayon
-            self.changey * -1
+            self.changey *= -1
 
         arcade.finish_render() # afficher l'ecran apres avoir mis a jour
 
     def draw(self):
-        arcade.draw_circle_filled(self.x, self.y, self.rayon, (random.randint(0,255), random.randint(0, 255), random.randint(0, 255))) # dessiner un cercle sur l'ecran
+        arcade.draw_circle_filled(self.x, self.y, self.rayon, self.color) # dessiner un cercle sur l'ecran
 
 
 class Rectangle:
     def __init__(self):
         self.x = None
         self.y = None
-        self.changex = None
-        self.changey = None
+        self.changex = random.randint(-5,5) # vitesse du rectangle en x aléatoire entre 1 et 5
+        self.changey = random.randint(-5,5) # vitesse du rectangle en y aléatoire entre 1 et 5
 
-        self.width = random.randint(10,30)
-        self.height = random.randint(10,30)
-        self.angle = random.randint(0, 359)
+        self.width = random.randint(10,30) # définir une largeur entre 10 et 30
+        self.height = random.randint(10,30) # définir une hauteur entre 10 et 30
+        self.angle = random.randint(0, 359) # définir l'angle du rectangle entre 0 et 359° (360 et 0 c'est la meme chose)
 
-        self.color = arcade.color
+        self.color = random.choice(COLORS) # définir une couleur au hasard dans le tuple
 
     def update(self):
-        if self.x > SCRN_W - self.width:
-            self.x = SCRN_W - self.width
-            self.changex = 0
+        self.x += self.changex
+        self.y += self.changey
 
-        if self.x < self.width:
+        if self.x > SCRN_W - self.width: # si le rectangle va plus loin que la bordure droite de l'ecran
+            self.x = SCRN_W - self.width # empèche le rectangle de sortir
+            self.changex *= -1 # changer la direction pour l'opposé
+
+        if self.x < self.width: # si le rectangle va plus loin que la bordure gauche de l'ecran
             self.x = self.width
-            self.changey = 0
+            self.changex *= -1
 
-        if self.y > SCRN_H - self.height:
+        if self.y > SCRN_H - self.height: # si le rectangle va plus loin que la bordure en haut de l'ecran
             self.y = SCRN_H - self.height
-            self.changey = 0
+            self.changey *= -1
 
-        if self.y < self.height:
+        if self.y < self.height: # si le rectangle va plus loin que la bordure en bas de l'ecran
             self.y = self.height
-            self.changey = 0
+            self.changey *= -1
 
-        arcade.finish_render()
+        arcade.finish_render() # afficher l'ecran apres avoir mis a jour
 
     def draw(self):
-
-        arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.color.WHITE, self.angle)
+        arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.color, self.angle) # dessiner un rectangle sur l'ecran
 
 #######################
 
